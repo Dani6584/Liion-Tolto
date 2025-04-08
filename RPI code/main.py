0d510037c5d9fe8b001c7c3bac12e3bda5bb86ed
Nagy Dániel <nagydaniel6584@gmail.com>
2025-04-08 14:06:17 +0200

    javitott

--- RPI code/main.py
+++ RPI code/main.py
@@ -218,20 +218,17 @@ def rotate_to_position(client, current, target):
 
     for i in range(n):
         client.write_coil(MODBUS_OUTPUT_STEPPER, 1)
-        time.sleep(0.5)
+        time.sleep(0.25)
         
         coils = client.read_coils(SENSOR_COIL_ADDRESS, count=1)
         while coils.bits[0] != True:
             coils = client.read_coils(SENSOR_COIL_ADDRESS, count=1)
-            if coils.bits[0] == True:
-                log_to_appwrite(f"ERZEKELTE {jel}")
         
-        time.sleep(0.1)
         client.write_coil(MODBUS_OUTPUT_STEPPER, 0)
 
         jel += 1
         log_to_appwrite(f"n: {n}, jel: {jel}")
-        time.sleep(0.5)
+        time.sleep(1)
     log_to_appwrite(f"Position reached: {target}")
 
 def do_loading_step(client, bid):
